140a59825f6ea4541721072d714e99e77482b150
David García-Navas López de Cuéllar <davidgarcianavas@gmail.com>
2015-10-04 10:35:44 +0200

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -1,7 +1,9 @@
 # name: plugin_discourse_sidebar
-# about: create and view status of zendesk ticket within each topic
+# about: add a dynamic sidebar on the right
+# version: 0.1
 # authors: Vairix
+# url: https://github.com/tcreativo/plugin_discourse_dynamic_sidebar
+
 register_asset "javascripts/admin/templates/site_settings/setting_text.hbs"
 register_asset "javascripts/discourse/templates/discovery.hbs"
-
 register_asset "stylesheets/sidebar_styles.css"
